a0155b0e974b7ab2f98d0c28ac1f516a6b89b5f9
Al <37927715+al-haras@users.noreply.github.com>
2021-02-13 09:19:04 -0800

    Update struct.go to have better formatting

--- cmd/struct.go
+++ cmd/struct.go
@@ -9,13 +9,13 @@ type Sample struct {
 	SHA256     	string    	`json:"sha256_hash"`
 	SHA384	   	string    	`json:sha3_384_hash`
 	SHA1		string		`json:sha1_hash`
-	MD5			string		`json:md5_hash`
+	MD5		string		`json:md5_hash`
 	FirstSeen	string		`json:first_seen`
 	LastSeen	string		`json:last_seen`
 	FileName	string		`json:file_name`
-	FileSize	int			`json:file_size`
+	FileSize	int		`json:file_size`
 	FileType	string		`json:file_type`
 	Reporter	string		`json:reporter`
-    Signature  	string    	`json:"signature"`
-    Tags       	[]string  	`json:"Tags"`
+    	Signature  	string    	`json:"signature"`
+    	Tags       	[]string  	`json:"Tags"`
 }
